1dabe70e2749068e09425a8c44ae71f26488a89a
Bradley Dufour <dufour.bradley@gmail.com>
2025-11-11 14:50:26 -0500

    Add to data class
    Add some more utility functions, make snoozing more dynamic. Manipulate
    use of Calendars vs Dates to limit potential manhandling of internal
    data from external sources, but allowing from within Todo

--- app/src/main/java/com/example/everydaytodolist/data/Todo.kt
+++ app/src/main/java/com/example/everydaytodolist/data/Todo.kt
@@ -4,26 +4,54 @@ import android.icu.util.Calendar
 import java.time.LocalTime
 import java.util.Date
 
+//TODO Interface for different types of Todos
 class Todo(
     var title: String = "New Todo", //TODO Get this into a string resource
     var frequencyInDays: Int = 1,
     var alarmTime: LocalTime = LocalTime.of(9,0)
 ) {
-
-    val calendar: Calendar = Calendar.getInstance()
-    var lastOccurrence: Date = calendar.time
-        private set(value) {
-            field = value
-        }
+    private var nextOccurrence: Calendar = calculateNextOccurrence()
+    private var lastOccurrence: Calendar = Calendar.getInstance()
     var timesSnoozedSinceLastCompletion: Int = 0
         private set(value) {
             field = value
         }
 
+    fun getLastOccurrenceTime(): Date {
+        return lastOccurrence.time
+    }
+    fun getNextOccurrenceTime(): Date {
+        return nextOccurrence.time
+    }
+
+    fun markCompleted() {
+        timesSnoozedSinceLastCompletion = 0
+        lastOccurrence = Calendar.getInstance()
+        nextOccurrence = calculateNextOccurrence()
+    }
+
+    fun snooze() {
+        timesSnoozedSinceLastCompletion++
+        nextOccurrence = calculateNextOccurrence(snoozeLength = 1)
+    }
+
+    private fun calculateNextOccurrence(
+        from: Calendar = Calendar.getInstance(),
+        snoozeLength: Int = frequencyInDays
+    ): Calendar {
+        val calendar = from
+        calendar.add(Calendar.DAY_OF_YEAR, snoozeLength)
+        calendar.set(Calendar.HOUR, alarmTime.hour)
+        calendar.set(Calendar.MINUTE, alarmTime.minute)
+        calendar.set(Calendar.SECOND, 0)
+        return calendar
+    }
+
     companion object Factory {
         fun copy(other: Todo): Todo {
             var todoCopy = Todo(other.title, other.frequencyInDays, other.alarmTime)
             todoCopy.lastOccurrence = other.lastOccurrence
+            todoCopy.nextOccurrence = todoCopy.calculateNextOccurrence(todoCopy.lastOccurrence)
             todoCopy.timesSnoozedSinceLastCompletion = other.timesSnoozedSinceLastCompletion
             return todoCopy
         }
